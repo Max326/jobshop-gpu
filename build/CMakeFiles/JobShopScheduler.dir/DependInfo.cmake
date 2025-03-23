1350ee2912437063781f81e03ef48a68e118d569
Max326 <maxconnor326@gmail.com>
2025-03-23 11:06:46 +0100

    cmake configured

---
+++ build/CMakeFiles/JobShopScheduler.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/Projects/jobshop-gpu/src/NeuralNetwork.cu" "CMakeFiles/JobShopScheduler.dir/src/NeuralNetwork.cu.o" "gcc" "CMakeFiles/JobShopScheduler.dir/src/NeuralNetwork.cu.o.d"
+  "/home/ubuntu/Projects/jobshop-gpu/src/JobShopHeuristic.cpp" "CMakeFiles/JobShopScheduler.dir/src/JobShopHeuristic.cpp.o" "gcc" "CMakeFiles/JobShopScheduler.dir/src/JobShopHeuristic.cpp.o.d"
+  "/home/ubuntu/Projects/jobshop-gpu/src/main.cpp" "CMakeFiles/JobShopScheduler.dir/src/main.cpp.o" "gcc" "CMakeFiles/JobShopScheduler.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
