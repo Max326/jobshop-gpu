04d9f36eb0a2babdfb4e96fe5c6e0bc364d9f6ca
Max326 <maxconnor326@gmail.com>
2025-05-14 18:32:17 +0200

    fixed merge

--- src/main_cmaes.cpp
+++ src/main_cmaes.cpp
@@ -2,22 +2,22 @@
 
 using namespace libcmaes;
 
-int main(int argc, char *argv[])
-{
-    int population_size = 192;
-    int nn_weights_count = 2; // ilosc wag w sieci np. 300                                                                          
-    std::vector<double> x0(nn_weights_count, 0.0);
-    double sigma = 0.1;
+// int main(int argc, char *argv[])
+// {
+//     int population_size = 192;
+//     int nn_weights_count = 2; // ilosc wag w sieci np. 300                                                                          
+//     std::vector<double> x0(nn_weights_count, 0.0);
+//     double sigma = 0.1;
 
-    CMAParameters<> cmaparams(x0, sigma, population_size);  
-    FitFunc eval = [](const double *x, const int N) -> double { return 0.0; };
-    ESOptimizer<customCMAStrategy,CMAParameters<>> optim(eval,cmaparams);
+//     CMAParameters<> cmaparams(x0, sigma, population_size);  
+//     FitFunc eval = [](const double *x, const int N) -> double { return 0.0; };
+//     ESOptimizer<customCMAStrategy,CMAParameters<>> optim(eval,cmaparams);
 
-    while(!optim.stop()) {
-        dMat candidates = optim.ask();
-        optim.eval(candidates);
-        optim.tell();
-        optim.inc_iter(); // important step: signals next iteration.                                              
-    }
-    std::cout << optim.get_solutions() << std::endl;
-}
+//     while(!optim.stop()) {
+//         dMat candidates = optim.ask();
+//         optim.eval(candidates);
+//         optim.tell();
+//         optim.inc_iter(); // important step: signals next iteration.                                              
+//     }
+//     std::cout << optim.get_solutions() << std::endl;
+// }
