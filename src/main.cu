f01737fb4791740b945f9ff1cbb8a9f6f13cbfc4
Max326 <maxconnor326@gmail.com>
2025-07-25 16:38:38 +0200

    second smallest dataset results

--- src/main.cu
+++ src/main.cu
@@ -44,20 +44,20 @@ int main(int argc, char *argv[]){
     int n = datasets.size();
 
     int start_index = 1;
-    int ds_amount = 5; // Number of datasets to process in this run
+    int ds_amount = 1; // Number of datasets to process in this run
 
     int max_loaded_problems = 130000;
 
     for (int i = start_index; i < start_index + ds_amount && i < n; ++i) {
-        if (i == 1 || i == 5){   
-            if (i >= 16) max_loaded_problems -= 20000; // Adjust for larger datasets
-            
-            std::string train_problem_file = "TRAIN/" + datasets[i] + "_total.json";
-            std::string validate_problem_file = "VALID/" + datasets[i] + "_validation.json";
-            std::string test_problem_file = "TEST/" + datasets[i] + "_test.json";
-            
-            main_cmaes(train_problem_file, validate_problem_file, test_problem_file, max_loaded_problems);
-        }
+        // if (i == 1 || i == 5){   
+        if (i >= 16) max_loaded_problems -= 20000; // Adjust for larger datasets
+        
+        std::string train_problem_file = "TRAIN/" + datasets[i] + "_total.json";
+        std::string validate_problem_file = "VALID/" + datasets[i] + "_validation.json";
+        std::string test_problem_file = "TEST/" + datasets[i] + "_test.json";
+        
+        main_cmaes(train_problem_file, validate_problem_file, test_problem_file, max_loaded_problems);
+        // }
     }
 
     return 0;
